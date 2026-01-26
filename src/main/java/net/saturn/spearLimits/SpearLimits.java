e84f83cc18a4d6ea79bc73f1a1d776ec89fa7aea
Pumpkin <wishsmp1002@gmail.com>
2026-01-26 20:16:30 +1100

    INITIAL COMMIT

---
+++ src/main/java/net/saturn/spearLimits/SpearLimits.java
@@ -0,0 +1,17 @@
+package net.saturn.spearLimits;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpearLimits extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
